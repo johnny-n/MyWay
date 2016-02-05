70ab0c598a50fa1278284bf7bfb1553b827721eb
Johnny Nguyen <johnnytn.mail@gmail.com>
2016-02-04 18:37:09 -0800

    Set up Directions API in Fragment

--- app/src/main/java/com/projects/johnny/myway/DirectionsFragment.java
+++ app/src/main/java/com/projects/johnny/myway/DirectionsFragment.java
@@ -13,17 +13,65 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.maps.DirectionsApi;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.DistanceMatrixApi;
+import com.google.maps.DistanceMatrixApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
+import com.google.maps.GeocodingApiRequest;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DistanceMatrix;
+import com.google.maps.model.DistanceMatrixElement;
+import com.google.maps.model.DistanceMatrixElementStatus;
+import com.google.maps.model.DistanceMatrixRow;
+import com.google.maps.model.Duration;
+import com.google.maps.model.GeocodingResult;
+import com.google.maps.model.TravelMode;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class DirectionsFragment extends Fragment {
 
-    Button mMiddleButton;
-    RecyclerView mRecyclerView;
+    private static final String GOOGLE_MAPS_API_REQUEST = "https://maps.googleapis.com/maps/api/directions/";
+
+    Button mTestButton;
+    int travelTime = 0;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_directions, container, false);
+
+        // An instance of GeoApiContext is required to use Google Maps API
+        final GeoApiContext context = new GeoApiContext().setApiKey("AIzaSyBUXYP-F-4uIdri-j9Y4RzdBf3stL325pY");
+
+        mTestButton = (Button) v.findViewById(R.id.button);
+        mTestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DirectionsApiRequest directionsApiRequest = DirectionsApi.newRequest(context)
+                        .origin("50 Washington St, Santa Clara, CA 95050")
+                        .destination("2641 Quail Dr, Union City, CA 94587")
+                        .mode(TravelMode.DRIVING);
+
+                try {
+                    DirectionsResult result = directionsApiRequest.await();
+                    long travelTime = result.routes[0].legs[0].steps[0].duration.inSeconds/60; // in minutes
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        Location from = new Location("Home", "50 Washington Street, Santa Clara, CA");
+        Location to = new Location("School", "San Jose State University, San Jose, CA");
 
 
 
