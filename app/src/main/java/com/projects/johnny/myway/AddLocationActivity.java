9fd58b7f5dfeb26930681cee14786808afe41229
Johnny Nguyen <johnnytn.mail@gmail.com>
2016-02-11 17:36:05 -0800

    Firebase Integration + UI stuff

--- app/src/main/java/com/projects/johnny/myway/AddLocationActivity.java
+++ app/src/main/java/com/projects/johnny/myway/AddLocationActivity.java
@@ -3,7 +3,11 @@ package com.projects.johnny.myway;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.firebase.client.Firebase;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
@@ -23,10 +27,10 @@ public class AddLocationActivity extends AppCompatActivity {
 
         // Set up Firebase in Android
         // Must be initialized once with an Android context
-//        Firebase.setAndroidContext(this);
-//        Firebase myFirebaseRef = new Firebase("https://myways.firebaseio.com/");
-//        myFirebaseRef.child("message").setValue("Hello, World!");
+        Firebase.setAndroidContext(this);
+        final Firebase myFirebaseRef = new Firebase("https://myways.firebaseio.com/");
 
+        // Obtain FragmentManager
         android.app.FragmentManager fm = getFragmentManager();
         // Get reference to PlaceAutocomplete UI widget
         mPlaceAutocompleteFragment = (PlaceAutocompleteFragment) fm.findFragmentById(R.id.place_autocomplete_fragment);
